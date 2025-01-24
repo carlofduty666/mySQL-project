cf34370a6a6f0e21ceda6b95cbd398504b19ce6a
Carlos <dschworld@gmail.com>
2025-01-24 10:42:31 -0400

    backup1

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users');
+const { User}  = require('../models/users');
 
 router.get('/', (request, response) => {
     User.getAllUsers((error, result) => {
