1c25d26c1846313ad7367903b96fe505704e8d32
Carlos <dschworld@gmail.com>
2025-02-06 19:39:11 -0400

    das war's fuer heute

--- public/js/app.js
+++ public/js/app.js
@@ -27,7 +27,7 @@ function getData(){
                     <span value="${user.id}">
                         ${user.fecha_creacion}
                     </span>
-                    <button value="${user.id}" onclick="editData('PUT', ${user.id})">
+                    <button value="${user.id}" onclick="editData(${user.id})">
                         Editar
                     </button>
                     <button value="${user.id}" onclick="deleteItem(${user.id})">
@@ -36,6 +36,11 @@ function getData(){
                         </div>
                         `
                     });
+    })
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        saveData();
     })
 
 }
@@ -57,7 +62,7 @@ function deleteItem(id){
     })
 }
 
-function editData(method, id) {
+function editData(id) {
     var nombre = document.getElementById('nombre');
     var apellido = document.getElementById('apellido');
     var correo = document.getElementById('correo');
@@ -65,13 +70,13 @@ function editData(method, id) {
     var numero_telefono = document.getElementById('numero_telefono');
 
     var user = document.getElementById(`user-card-${id}`);
-    nombre.value = user.querySelector('.nombre').innerText;
-    apellido.value = user.querySelector('.apellido').innerText;
-    correo.value = user.querySelector('.correo').innerText;
-    direccion.value = user.querySelector('.direccion').innerText;
+    nombre.value = user.getElementsByClassName('nombre')[0].innerText;
+    apellido.value = user.getElementsByClassName('apellido')[0].innerText;
+    correo.value = user.getElementsByClassName('correo')[0].innerText;
+    direccion.value = user.getElementsByClassName('direccion')[0].innerText;
     // numero_telefono.value = document.querySelector('.numero_telefono')[0].innerText;
 
-    let rawTelefono = user.querySelector('.numero_telefono')?.innerText || "";
+    let rawTelefono = user.getElementsByClassName('numero_telefono')?.innerText || "";
     numero_telefono.value = rawTelefono.trim(); // Remove leading/trailing spaces!
 
 
@@ -82,7 +87,7 @@ function editData(method, id) {
 
     btn.addEventListener('click', (e) => {
         e.preventDefault();
-        saveData(method, id);
+        saveData(id);
     })
 }
 
@@ -138,7 +143,7 @@ function editData(method, id) {
     
 // }
 
-function saveData(method, id = "") {
+function saveData(id = "") {
     
 var nombre = document.getElementById('nombre');
 var apellido = document.getElementById('apellido');
@@ -146,37 +151,64 @@ var correo = document.getElementById('correo');
 var direccion = document.getElementById('direccion');
 var numero_telefono = document.getElementById('numero_telefono');
 
-fetch('http://localhost:9999/user', {
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        nombre: nombre.value,
-        apellido: apellido.value,
-        correo: correo.value,
-        direccion: direccion.value,
-        numero_telefono: numero_telefono.value
+if (id == "") {
+    fetch('http://localhost:9999/user', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nombre: nombre.value,
+            apellido: apellido.value,
+            correo: correo.value,
+            direccion: direccion.value,
+            numero_telefono: numero_telefono.value
+        })
     })
-})
-.then(response => {
-    if (response.ok) {
-        alert('Usuario creado correctamente')
-        getData();
-        nombre.value = '';
-        apellido.value = '';
-        correo.value = '';
-        direccion.value = '';
-        numero_telefono.value = '';
-    } else {
-        alert('Error al crear el usuario')
-    }
-    return response.json()
-})
-.then(data => {
-    console.log(data)
-})
+    .then(response => {
+        if (response.ok) {
+            alert('Usuario creado correctamente')
+            getData();
+            nombre.value = '';
+            apellido.value = '';
+            correo.value = '';
+            direccion.value = '';
+            numero_telefono.value = '';
+        } else {
+            alert('Error al crear el usuario')
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
 
-}
+} else {
+    fetch('http://localhost:9999/user', {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id,
+            nombre: nombre.value,
+            apellido: apellido.value,
+            correo: correo.value,
+            direccion: direccion.value,
+            numero_telefono: numero_telefono.value
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Usuario creado correctamente')
+            getData();
+            nombre.value = '';
+            apellido.value = '';
+            correo.value = '';
+            direccion.value = '';
+            numero_telefono.value = '';
+        }
+    })
 
 
+}}
